60bb07958fac4d817cd031f87606c881c0b8bd09
meng <daniel.zhou@clickdishes.com>
2019-01-14 07:31:58 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import OtherPage from './other-page';
+import Fib from './fib';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+           <div>
+
+          <div className="App">
+             <Link to='/fib'>Home</Link>
+             <Link to='/otherpage'>Other Page</Link>
+           </div>
+           <div>
+             <Route exact  path='/fib' component={Fib} />
+             <Route exact  path='/otherpage' component={OtherPage} />
+           </div>
+           </div>
+      </Router>  
+    );
+  }
+}
+
+export default App;
---
+++ client/src/other-page.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class OtherPage extends Component {
+    render() {
+        return <div>
+            <Link to='/'>Go to Home</Link> 
+        </div>
+    }
+}
